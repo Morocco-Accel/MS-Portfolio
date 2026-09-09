3636aa52311736d6bc72be8076519b71fb9f5480
Morocco-Accel <marouane.shaimi94@gmail.com>
2026-09-09 12:22:46 +0100

    V2

--- components/TechCarousel/TechCarouselClient.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-// The R3F canvas relies on WebGL/DOM APIs, so it's loaded client-only.
-const TechCarousel = dynamic(() => import("./TechCarousel"), { ssr: false });
-
-export default TechCarousel;
--- components/TechCarousel/react-three-fiber-jsx.d.ts
+++
@@ -1,8 +0,0 @@
-import type { ThreeElement } from "@react-three/fiber";
-import type { BentPlaneGeometry } from "./util";
-
-declare module "@react-three/fiber" {
-  interface ThreeElements {
-    bentPlaneGeometry: ThreeElement<typeof BentPlaneGeometry>;
-  }
-}
